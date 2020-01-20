d0bf13d1ba4d64cdbdde3e5c8a483881e9c3a2f9
Sergey <xxsmotur@gmail.com>
2020-01-20 21:25:37 +0500

    implementation of the query method

--- algorithms/src/tree/fenwick.rs
+++ algorithms/src/tree/fenwick.rs
@@ -9,4 +9,17 @@ impl FenwickTree {
             data: [i32;size],
         }
     }
+
+    pub fn query(&self, idx:i32) -> i32 {
+        fn res(result:i32, i:i32) -> i32 {
+            match i {
+                0 => result,
+                _ => {
+                    res(result+self.data[i], i -= i & (-i))
+                }
+            }
+        }
+
+        return res(0, idx)
+    }
 }
