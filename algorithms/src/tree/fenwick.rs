6e04e9f24727371bb6b8c5164c8ed4ee99a52317
Sergey <xxsmotur@gmail.com>
2020-01-21 16:51:34 +0500

    fenwick tree: fixes

--- algorithms/src/tree/fenwick.rs
+++ algorithms/src/tree/fenwick.rs
@@ -1,25 +1,25 @@
 
 pub struct FenwickTree {
-    data: [i32],
+    data: Vec<i32>,
 }
 
 impl FenwickTree {
     pub fn new(size:i32) -> FenwickTree {
         Self {
-            data: [i32;size],
+            data: vec![0:size],
         }
     }
 
     pub fn query(&self, idx:i32) -> i32 {
-        fn res(result:i32, i:i32) -> i32 {
+        fn res(ft: &FenwickTree, result:i32, i:i32) -> i32 {
             match i {
                 0 => result,
                 _ => {
-                    res(result+self.data[i], i -= i & (-i))
+                    res(result+ft.data[i], i -= i & (-i))
                 }
             }
         }
 
-        return res(0, idx)
+        return res(self, 0, idx)
     }
 }
